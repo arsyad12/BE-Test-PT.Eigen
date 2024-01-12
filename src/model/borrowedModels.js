b4548a512f19449ba6937fcaf4db91fd39cc2d6e
Arsyad Basofil Amri <33723389+arsyad12@users.noreply.github.com>
2024-01-12 07:45:13 +0700

    [UPDATE] borrowed model

--- src/model/borrowedModels.js
+++ src/model/borrowedModels.js
@@ -35,19 +35,42 @@ const borrowedModel = {
     }
   },
 
+  checkPenalty: async (payload) => {
+    try {
+      const { date_return } = payload;
+      const checkPenalty = prisma.borrowed.findMany({
+        where: {
+          date_return : date_return
+        },
+        select:{
+          date_return : true
+        }
+      });
+      return checkPenalty;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
 
 
   addData: async (payload) => {
     try {
       const { uuid_member, uuid_book } = payload;
 
+      
+      const borrowDate = new Date();
+      const getDates = new Date();
+      const returnDate = new Date(getDates.setDate(getDates.getDate() + 6));
+
       const request = await prisma.borrowed.create({
         data: {
           uuid_member: uuid_member,
           uuid_book: uuid_book,
+          date_borrwoed : borrowDate,
+          date_return : returnDate
         },
       });
-
 
 
       if (request) {
@@ -68,6 +91,57 @@ const borrowedModel = {
       console.log(error);
     }
   },
+
+  deleteData: async (payload) => { //for return  
+    try {
+      const { uuid_book } = payload;
+
+
+      const request = await prisma.borrowed.delete({
+        where: {
+          uuid_book: uuid_book,
+        },
+      });
+
+
+      if (request) {
+        const updateStock = await prisma.book.update({
+          where: {
+            uuid_book: uuid_book,
+          },
+          data: {
+            stock: { increment: 1 },
+          },
+        });
+
+        return updateStock;
+      }
+
+      return request;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+
+  givePenalty: async (payload) => {
+
+    const{uuid_member} = payload
+
+    const request = await prisma.member.update({
+        where:{
+          uuid_member : uuid_member
+        },
+        data:{
+          isMemberPenalize : true
+        }
+    })
+
+    return request
+
+  }
+
+
 };
 
 module.exports = borrowedModel;
