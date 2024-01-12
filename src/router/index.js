c08b0f817779e59699d9a9b09d70c93ccbb108e0
Arsyad Basofil Amri <33723389+arsyad12@users.noreply.github.com>
2024-01-13 05:34:20 +0700

    [UPDATE] router

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,25 @@
 const express = require('express')
 const router = express.Router()
 
+
 const memberController = require('../controller/memberControllers')
 const bookController = require('../controller/bookControllers')
 const borrowedController = require('../controller/borrowedControllers')
 
+
 //endpoint member
-router.post('/member',memberController._addMember)
+router.post('/member/register',memberController._addMember)
 router.post('/member/login',memberController._loginMember)
 router.get('/member',memberController._getDataMember)
 
 //endpoint book
 router.post('/book',bookController._addBook)
+router.get('/book/getAllData',bookController._getBook)
 
 //endpoint borrowed
 router.post('/borrowed',borrowedController._addBorrowed)
-router.delete('/borrowed',borrowedController._deleteBorrowed)
+router.get('/borrowed/getListBorrowed',borrowedController._getBorrowed)
+router.delete('/borrowed/return',borrowedController._deleteBorrowed)
 
 
 module.exports = router
